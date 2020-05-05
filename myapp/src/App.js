3c541f78edbbf7b38781fad8af88476bccd1105e
Aaron Yan <>
2020-05-04 22:57:47 -0700

    Add products info

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,24 +1,133 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import "./App.css";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import AppBar from "./components/AppBar";
+import LinksSlide from "./components/LinksSlide";
+import ShopCart from "./components/ShopCart";
+import ContactInfo from "./components/ContactInfo";
+import ShoppingCartOutlinedIcon from "@material-ui/icons/ShoppingCartOutlined";
+import Popup from "reactjs-popup";
+
+const useStyles = makeStyles((theme) => ({
+  quantity: {
+    // height: 20,
+    // width: 20,
+  },
+}));
 
 function App() {
+  const classes = useStyles();
+  const [selectedProduct1, setSelectedProduct1] = useState(0);
+  const [selectedProduct2, setSelectedProduct2] = useState(0);
+  const [selectedProducts, setSelectedProducts] = useState(0);
+
+  const addToCartHandler = () => {
+    setSelectedProducts(
+      Number.parseInt(selectedProduct1) + Number.parseInt(selectedProduct2)
+    );
+  };
+
+  const handleProduct1Change = (e) => {
+    setSelectedProduct1(e.target.value);
+  };
+
+  const handleProduct2Change = (e) => {
+    setSelectedProduct2(e.target.value);
+  };
+  // console.log("p1", selectedProduct1);
+  // console.log("p2", selectedProduct2);
+  // console.log("total", selectedProducts);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App" data-testid="App">
+      <header className="App-header" data-testid="App-header">
+        <AppBar />
       </header>
+      <LinksSlide />
+      <div id="products-info" data-testid="products-info">
+        <div className="product" id="product1" data-testid="product1">
+          <div data-testid="product1-img">
+            <img src="https://whatsmorestudio.com/Soymilk_Custard_Box_Cake.png" />
+          </div>
+          <div className="product-description">
+            <Typography variant="subtitle1" data-testid="product1-description">
+              Blue Buffalo Healthy Gourmet
+            </Typography>
+          </div>
+          <form>
+            <div className="input-container">
+              <TextField
+                className="quantity"
+                size="small"
+                label="Quantity"
+                type="number"
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                InputProps={{ inputProps: { min: 0, max: 10 } }}
+                onChange={handleProduct1Change}
+              />
+              <Button
+                variant="contained"
+                onClick={addToCartHandler}
+                data-testid="product1-button"
+              >
+                Add to cart
+              </Button>
+            </div>
+          </form>
+        </div>
+
+        <div className="product" id="product2" data-testid="product2">
+          <div data-testid="product2-img">
+            <img src="https://whatsmorestudio.com/Soymilk_Custard_Box_Cake.png" />
+          </div>
+          <div className="product-description">
+            <Typography variant="subtitle1" data-testid="product2-description">
+              Blue Buffalo Healthy Gourmet
+            </Typography>
+          </div>
+          <form>
+            <div className="input-container">
+              <TextField
+                className="quantity"
+                size="small"
+                label="Quantity"
+                type="number"
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+                InputProps={{ inputProps: { min: 0, max: 10 } }}
+                onChange={handleProduct2Change}
+              />
+              <Button
+                variant="contained"
+                onClick={addToCartHandler}
+                className="product-button"
+                data-testid="product2-button"
+              >
+                Add to cart
+              </Button>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div id="shop-cart" data-testid="shop-cart">
+        <ShopCart selectedProducts={selectedProducts} />
+      </div>
+      <div id="cart-icon" data-testid="ShoppingCartRoundedIcon">
+      <Popup trigger={<ShoppingCartOutlinedIcon style={{ fontSize: 50 }} />} position="left center">
+        <div>Popup content here !!</div>
+      </Popup>
+        
+      </div>
+      <div data-testid="ContactInfo">
+        <ContactInfo />
+      </div>
     </div>
   );
 }
--- myapp/src/App.test.js
+++ myapp/src/App.test.js
@@ -6,4 +6,18 @@ test('renders App page', () => {
   const { getByTestId } = render(<App />);
   expect(getByTestId('App')).toBeInTheDocument();
   expect(getByTestId('App-header')).toBeInTheDocument();
+  expect(getByTestId('products-info')).toBeInTheDocument();
+
+  expect(getByTestId('product1')).toBeInTheDocument();
+  expect(getByTestId('product1-description')).toBeInTheDocument();
+  expect(getByTestId('product1-button')).toBeInTheDocument();
+  
+  expect(getByTestId('product2')).toBeInTheDocument();
+  expect(getByTestId('product2-description')).toBeInTheDocument();
+  expect(getByTestId('product2-button')).toBeInTheDocument();
+
+  expect(getByTestId('shop-cart')).toBeInTheDocument();
+  expect(getByTestId('ShoppingCartRoundedIcon')).toBeInTheDocument();
+
+  expect(getByTestId('ContactInfo')).toBeInTheDocument();
 });
