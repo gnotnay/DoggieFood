1ae2174fa9a17b2b08f250dc1f872d1af3d19d89
Aaron Yan <>
2020-05-04 22:57:28 -0700

    Add contact info

---
+++ myapp/src/components/ContactInfo.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Typography from "@material-ui/core/Typography";
+
+const ContactInfo = () => {
+  return (
+    <div>
+      <Typography>
+      Cake Designing Studio
+      </Typography>
+      <Typography>
+        whaT's More Cake
+      </Typography>
+      <Typography>
+      <a href="info@whatsmorestudio.com">info@whatsmorestudio.com</a>
+      </Typography>
+      <Typography>
+        500 Lawrence Expy, Sunnyvale, CA 94085
+      </Typography>
+      <Typography>
+        © 2020 whaT's More Cake
+      </Typography>
+    </div>
+  );
+}
+
+export default ContactInfo;
