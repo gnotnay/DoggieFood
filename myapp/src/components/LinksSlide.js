b8ba20d999df28fbc88d400d392cb15f19800c60
Aaron Yan <>
2020-05-12 10:07:33 -0700

    Add youtube ins links

--- myapp/src/components/LinksSlide.js
+++ myapp/src/components/LinksSlide.js
@@ -7,15 +7,12 @@ const LinksSlide = () => {
 
   return (
     <Typography className='links'>
-      <Link href="#" onClick={preventDefault} id='link1' data-testid="link1">
-        WhatsMore
-      </Link>
-      <Link href="#" onClick={preventDefault} id='link2' data-testid="link2">
-        Order
-      </Link>
-      <Link href="#" onClick={preventDefault} id='link3' data-testid="link3">
-        Gallery
-      </Link>
+      <a href="https://www.instagram.com/juliotheshiba/" target="_black" id='link3' style={{ textDecoration: 'none' }} data-testid="link3">
+      <img src="http://icode4.life/img/202005/instagram.png" alt="logo" height="25px" width="25px" id="instagram-img" data-testid="instagram-img"/>
+      </a>
+      <a href="https://www.youtube.com/c/JulioShiba/?sub_confirmation=1" target="_black" id='link3' style={{ textDecoration: 'none' }} data-testid="link3">
+      <img src="http://icode4.life/img/202005/youtube.png" alt="logo" height="60px" width="60px" id="youtube-img" data-testid="youtube-img"/>
+      </a>
     </Typography>
   );
 }
